eca74318257f4351e0d566f2a592b5bf2a262e5e
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-08-25 22:57:44 -0400

    request body

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import string
-# Import FastAPI
+from typing import Union
+
 from fastapi import FastAPI
 
-# Create an instance of fastapi
 app = FastAPI()
 
-# Create an operational decorator
-@app.get("/")
-# Create an operational funtion
-async def root():
-    return {"message": "Hello World"}
 
-# Create an operation decorator (url)
 @app.get("/items/{item_id}")
-# Create an operational function
-async def read_item(item_id):
-    return {"item_id": item_id}
-
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
