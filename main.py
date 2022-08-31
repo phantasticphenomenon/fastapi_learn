2889bfcbd92650f99b2110d857c1ae6560e07b55
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-08-30 23:28:37 -0400

    from typing import Union

--- main.py
+++ main.py
@@ -19,6 +19,15 @@ class User(BaseModel):
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(gt=0),
+    q: Union[str, None] = None
+):
     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
     return results
