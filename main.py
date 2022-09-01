2d67347be48dd4d8ad27f5bac857a4cb3d1aa35c
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-08-31 23:05:13 -0400

    Field additional arguments

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        }
+    name: str = Field(example="Foo")
+    description: Union[str, None] = Field(default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Union[float, None] = Field(default=None, example=3.2)
 
 
 @app.put("/items/{item_id}")
