d3cb7d4dcf24949fcacfe2e896abb7362ea67621
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-08-31 23:22:04 -0400

    import and declare cookie parameters

--- main.py
+++ main.py
@@ -1,51 +1,10 @@
 from typing import Union
 
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Item = Body(
-        examples={
-            "normal": {
-                "summary": "A normal example",
-                "description": "A **normal** item works correctly.",
-                "value": {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                },
-            },
-            "converted": {
-                "summary": "An example with converted data",
-                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                "value": {
-                    "name": "Bar",
-                    "price": "35.4",
-                },
-            },
-            "invalid": {
-                "summary": "Invalid data is rejected with an error",
-                "value": {
-                    "name": "Baz",
-                    "price": "thirty five point four",
-                },
-            },
-        },
-    ),
-):
-    results = {"item_id": item_id, "item": item}
-    return results
+@app.get("/items/")
+async def read_items(ads_id: Union[str, None] = Cookie(default=None)):
+    return {"ads_id": ads_id}
