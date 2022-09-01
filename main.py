c5f4d6057fdf4eed18f69a654e5d6491f87f8ebe
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-08-31 23:34:14 -0400

    Response model

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from typing import Union
+from typing import List, Union
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(user_agent: Union[str, None] = Header(default=None)):
-    return {"User-Agent": user_agent}
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: List[str] = []
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
