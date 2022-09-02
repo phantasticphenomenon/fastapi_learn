70ee5f8402508c1533feefb92067ad291d057e3f
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-09-01 22:07:04 -0400

    return same input data

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from typing import List, Union
+from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: List[str] = []
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
 
 
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
-    return item
+# Don't do this in production!
+@app.post("/user/", response_model=UserIn)
+async def create_user(user: UserIn):
+    return user
