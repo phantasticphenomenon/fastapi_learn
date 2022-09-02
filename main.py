163340def433198780ecfa7e57613072f688dc36
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-09-01 22:20:03 -0400

    response model encoding parameters

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-from typing import Union
+from typing import List, Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: Union[str, None] = None
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: float = 10.5
+    tags: List[str] = []
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Union[str, None] = None
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
 
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
