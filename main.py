8605707f8ea0a558d28e702010a442cddbc58a07
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-09-02 23:22:51 -0400

    Optional file upload

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI, Form
+from typing import Union
+
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
-@app.post("/login/")
-async def login(username: str = Form(), password: str = Form()):
-    return {"username": username}
+@app.post("/files/")
+async def create_file(file: Union[bytes, None] = File(default=None)):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: Union[UploadFile, None] = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
