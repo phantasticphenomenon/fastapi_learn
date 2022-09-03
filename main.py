0c5a53fa65fa52762e59270d1b96afdb5a4ee443
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-09-02 23:24:34 -0400

    UploadFile with additional Metadata

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from typing import Union
-
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
 @app.post("/files/")
-async def create_file(file: Union[bytes, None] = File(default=None)):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
+async def create_file(file: bytes = File(description="A file read as bytes")):
+    return {"file_size": len(file)}
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: Union[UploadFile, None] = None):
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"filename": file.filename}
+async def create_upload_file(
+    file: UploadFile = File(description="A file read as UploadFile"),
+):
+    return {"filename": file.filename}
