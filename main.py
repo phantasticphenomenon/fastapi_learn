400ed0546f6a50bff3c936dca6c1650ecf765c78
kagiso Phaphe <kphaphe@host1.abixio.com>
2022-09-03 17:41:50 -0400

    response description

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ class Item(BaseModel):
     "/items/",
     response_model=Item,
     summary="Create an item",
-    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+    response_description="The created item",
 )
 async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
     return item
